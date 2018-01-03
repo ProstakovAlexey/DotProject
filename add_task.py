a6df0d3b01de704d258cc7e59e8e825074e7b8cc
ProstakovAlexey <alexey@tulalinux.ru>
2018-01-03 15:07:21 +0300

    Работаю над добавлением заданий. Пишу add_task, парралельно требуются новые параметры в конфиге и читалка для него. Добавил тесты, чтобы проверять их работу. Демо проект разместил в project_files. Сейчас add_task еще не работает, делаю пуш, чтобы перенести результаты домой.

--- add_task.py
+++ add_task.py
@@ -3,43 +3,123 @@ import config
 import pypyodbc
 import platform
 import datetime
-import obr_analiz
-import csv
+import leaders
+import project_files
 import re
+import glob
+import os
+import csv
 
 
 """
 Добавляет задания из файла CSV
 """
-users_id = {'alex': 2, 'dima': 345, 'andrey': 20}
 sh = re.compile(r'\(.*\)')
 
 
+def get_name(tj_name):
+    """
+    Получает имя пользователя из формата tj. Фактически удаляя кусок с скобками
+    :param tj_name:
+    :return: user_name
+    """
+    user_name_list = list()
+    for i in tj_name.split(sep=' '):
+        if not (i.find('(')>-1 and i.find(')')>-1):
+            # Это не кусок со скобками, любимый tj, его буду включать
+            user_name_list.append(i)
+    return ' '.join(user_name_list)
+
+def get_dpr_id(name, users):
+    """
+    Ищет id пользователя ДПР, по его имени
+    :param name: имя из csv файла
+    :param users: словарь с пользователя
+    :return: id или 0 (если не нашел)
+    """
+    dpr_id = 0
+    name = name.lower()
+    for user in users:
+        if user['name'].lower() == name:
+            dpr_id = user['id']
+            break
+    return dpr_id
+
+
 if __name__ == '__main__':
-    DB, users, work, err = config.readConfig()
-    print(users)
+    DB, users, work, imap, err = config.readConfig()
     if err:
         exit(1)
     # Соединение с БД
-    con = obr_analiz.getConnection(DB)
-    # Читает данные из CSV файла
-    csvfile = open('csv.csv', encoding='utf-8', newline='\n')
-    task_reader = csv.reader(csvfile, delimiter=';')
-    for i in task_reader:
-        if i[2] is not None:
-            # Не равен нулю, проверим какой id
-            name = sh.findall(i[2])[0]
-            if name is not None:
-                name = name.replace('(', '').replace(')', '')
-                if name is users_id:
-                    dpr_id = users_id[name]
-                    '''
+    #con = leaders.get_сonnect(DB)
+    # Находим все project_files файлы в каталоге project_files
+    for file in glob.glob('project_files/*.csv'):
+        csv_file_name = os.path.split(file)[1]
+        # Проверим, если для него конфигурационный файл, если нет, то не буду обрабатывать
+        ini_file_name = csv_file_name.replace('.csv', '.ini')
+        if not os.path.exists(os.path.join('project_files', ini_file_name)):
+            print('Для файла {0} нет файла описателя {1}. Его обработка не будет выполнена.'
+                  .format(csv_file_name, ini_file_name))
+            compile()
+        print('Выполняю обработку файла {0} и его описателя {1}'.format(csv_file_name, ini_file_name))
+
+        # Прочитаем свойства проекта
+        project_info, err_msg = config.read_proj_info(os.path.join('project_files', ini_file_name))
+        if err_msg:
+            print('Ошибка при обработке {} - {}. Проект будет пропущен.'.format(ini_file_name, err_msg))
+            continue
+
+        # Читаю csv файл
+        csv_file = open(os.path.join('project_files', csv_file_name), encoding='utf-8', newline='\n')
+        task_reader = csv.reader(csv_file, delimiter=';')
+        # Этот словарь накапливает информацию для создания заданий, я сразу их не создаю, т.к. это может
+        # быть опасно. Создание только после подтверждения от пользователя
+        tasks_for_dpr = list()
+        for i in task_reader:
+            if i[2]:
+                # Если исполнитель не равен равен нулю, то поищем его id
+                csv_user_name = get_name(i[2])
+                dpr_id = get_dpr_id(csv_user_name, users)
+                if dpr_id == 0:
+                    print('Пользователя [%s] не нашли в ДПР. Возможно у него перепутано имя, проверьте' % csv_user_name)
+                    continue
+                task_for_dpr = dict()
+                # Доработка
+                task_for_dpr['WorkTypeId'] = 1
+                # Название задания
+                task_for_dpr['name'] = i[1].strip()
+                # Кто тестирует
+                task_for_dpr['UserTestId'] = 2
+                # Кто делает
+                task_for_dpr['UserDevelopId'] = dpr_id
+                # Статус задания 1 - задание выдано
+                task_for_dpr['StatusId'] = 1
+                # Какой проект
+                task_for_dpr['ProjectId'] = project_info['id']
+                # ИД региона
+                task_for_dpr['regionId'] = project_info['name']
+                tasks_for_dpr.append(task_for_dpr)
+        csv_file.close()
+        # Будем добавлять задания
+
+        # Проверить, вдруг такое задание уже есть. В этом случае добавлять не будем
+
+        # Если задания нет, то показать пользователю что получилось
+
+        # и спросить добавлять или нет
+
+        # добавляем задание
+
+        # добавляем к нему комментарий
+
+
+        '''
                       insert Tasks (WorkTypeId, name, UserStartId , UserTestId, UserDevelopId, StatusId, ProjectId, dateStart, planDate)
 VALUES (1, "Название задания", 2, 2, 'кто разработчик', 1, 'название проекта', 'дата выдачи', 'дата выполнения');
                     комментарий -   insert TaskComment (taskid, comment, dt, userid) VALUES (1, '', '', 2)
 
-                    '''
+        '''
 
 
-    csvfile.close()
-    con.close()
+        #con.close()
+        #"""
